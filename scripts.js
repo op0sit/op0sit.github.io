d849d90306a2ed5213d8468d17b9bad136b56c33
op0sit <moltas@borggren.eu>
2025-08-10 16:55:18 +0200

    Got added to bucketwebring so added embed

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-const lightbox = document.getElementById('big_image');
-const lightboxImg = document.getElementById('big_image-img');
+const Bigimage = document.getElementById('bigimage');
+const BigimageImg = document.getElementById('bigimage-img');
 
 document.querySelectorAll('.gallery img').forEach(img => {
 img.addEventListener('click', () => {
-    lightbox.style.display = 'flex';
-    lightboxImg.src = img.src;
+    Bigimage.style.display = 'flex';
+    BigimageImg.src = img.src;
 });
 });
 
 // Close the big image when you click outside it
-lightbox.addEventListener('click', () => {
-lightbox.style.display = 'none';
+Bigimage.addEventListener('click', () => {
+Bigimage.style.display = 'none';
 });
